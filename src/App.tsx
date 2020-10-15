95f7ee316eca8001a8faccf6bd8913535323ecad
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-10-15 11:46:10 +0200

    I2/J9 Add simple implementation of Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import UsersList from './Users/components/UsersList';
+import TodoList from './TodoList/components/TodoList';
 
 function App() {
   return (
-    <div>
-      <UsersList />
-    </div>
+    <Switch>
+      <Route path="/users" component={UsersList} />
+      <Route path="/" component={TodoList} />
+    </Switch>
   );
 }
 
