23f63b69b2202e87a7632ce80abc62c53e4911c4
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-10-16 10:08:52 +0200

    I2/J10 Add LoginForm logic and inputs

--- src/Login/components/LoginForm.tsx
+++ src/Login/components/LoginForm.tsx
@@ -1,9 +1,59 @@
+import { Button, TextField } from '@material-ui/core';
 import React from 'react';
+import { login } from '../../api/users';
 
-class LoginForm extends React.Component {
+interface LoginFormState {
+  email: string;
+  password: string;
+}
+class LoginForm extends React.Component<{}, LoginFormState> {
+  constructor(props: {}){
+    super(props)
+    this.state = {
+      email: "",
+      password: ""
+    }
+  }
+
+  handleChange = (field: "email" | "password", newValue: string) => {
+    this.setState({
+      ...this.state,
+      [field]: newValue
+    })
+  }
+
+  handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    login(this.state.email, this.state.password).then((user) => alert(user.firstname));
+  }
+
   render(){
     return (
-      <h1>Connexion</h1>
+      <form onSubmit={this.handleSubmit} >
+        <TextField
+          required
+          label="Email"
+          variant="outlined"
+          value={this.state.email}
+          onChange={(e) => this.handleChange("email", e.target.value)}
+          />
+        <TextField
+          required
+          type="password"
+          label="Password"
+          variant="outlined"
+          value={this.state.password}
+          onChange={(e) => this.handleChange("password", e.target.value)}
+          />
+        <Button
+          type="submit"
+          color="primary"
+          variant="contained"
+        >
+          Submit
+        </Button>
+      </form>
     )
   }
 }
--- src/api/users.ts
+++ src/api/users.ts
@@ -4,3 +4,13 @@ import { IUser } from "../Users/types";
 export function getUsers(): Promise<IUser[]> {
   return axios.get('http://localhost:3000/api/users/').then(res => res.data);
 }
+
+export function login(email: string, password: string): Promise<IUser>{
+  return axios.post(
+    'http://localhost:3000/api/login/',
+    {
+      username: email,
+      password: password
+    }
+  ).then(res => res.data)
+}
