f3c79ab4a117788232e5ee8cbf9a49f6be41d2fc
daveguerra <david.guerradsc@gmail.com>
2020-10-16 10:10:44 +0200

    start loginForm

--- src/Login/components/LoginForm.tsx
+++ src/Login/components/LoginForm.tsx
@@ -1,11 +1,64 @@
-import React from 'react';
+//import { Button } from '@material-ui/core';
+import classes from '*.module.css';
+import { Button, TextField } from '@material-ui/core';
+import { SpeedRounded } from '@material-ui/icons';
+import React, { Fragment } from 'react';
+import { login } from '../../api/users';
 
-class LoginForm extends React.Component {
-  render(){
-    return (
-      <h1>Connexion</h1>
-    )
-  }
+
+interface LoginFormState {
+    email: string;
+    password: string;
+}
+
+class LoginForm extends React.Component<{}, LoginFormState> {
+
+    constructor(props: {}) {
+        super(props)
+        this.state = {
+            email: "",
+            password: ""
+        }
+    }
+
+    handleChange = (field: string, newValue: string) => {
+        this.setState({
+            ...this.state,
+            [field]: newValue
+        })
+    }
+
+    handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        login(this.state.email, this.state.password).then((user) => alert(user.firstname));
+    }
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit} >
+                <TextField
+                    required
+                    label="Email"
+                    variant="outlined"
+                    value={this.state.email}
+                    onChange={(e) => this.handleChange("email", e.target.value)}
+                />
+                <TextField
+                    required
+                    type="password"
+                    label="Password"
+                    variant="outlined"
+                    value={this.state.password}
+                    onChange={(e) => this.handleChange("password", e.target.value)}
+                />
+                <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained">
+                    Submit
+                </Button>
+            </form>
+        )
+    }
 }
 
 export default LoginForm;
--- src/api/users.ts
+++ src/api/users.ts
@@ -4,3 +4,13 @@ import { IUser } from "../Users/types";
 export function getUsers(): Promise<IUser[]> {
   return axios.get('http://localhost:3000/api/users/').then(res => res.data);
 }
+
+export function login(email: string, password: string): Promise<any>{
+    return axios.post(
+        'http://localhost:3000/api/login',
+        {
+            username: email,
+            password: password
+        }
+    )
+    }
