3e8272d80586799c8f215dfda79d3c12bdeb672f
daveguerra <david.guerradsc@gmail.com>
2020-10-19 11:54:41 +0200

    update profile

--- src/api/users.ts
+++ src/api/users.ts
@@ -5,12 +5,22 @@ export function getUsers(): Promise<IUser[]> {
     return axios.get('http://localhost:3000/api/users/').then(res => res.data);
 }
 
+export function getConnectedUser(): Promise<IUser> {
+  return axios.get(
+    'http://localhost:3000/api/users/me',
+    { withCredentials: true }
+  ).then(res => res.data);
+}
+
 export function login(email: string, password: string): Promise<IUser>{
   return axios.post(
     'http://localhost:3000/api/login/',
     {
       username: email,
       password: password
+    },
+    { 
+      withCredentials: true
     }
   ).then(res => res.data)
 }
