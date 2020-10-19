c8b557dafbb78363a64d3f0670b73ce5778eeafa
Thomas Falcone <32480223+ThomasF34@users.noreply.github.com>
2020-10-19 16:23:45 +0200

    I3/J11 Add Password requirements components

--- src/Login/components/CredentialSection.tsx
+++ src/Login/components/CredentialSection.tsx
@@ -1,6 +1,7 @@
-import { createStyles, TextField, Theme, Typography, withStyles } from '@material-ui/core';
-import React, { Fragment } from 'react';
+import { createStyles, TextField, Theme, withStyles, FormControl, FormLabel, FormGroup } from '@material-ui/core';
+import React from 'react';
 import { IFormField, IPasswordField, RegisterFormKey } from '../types';
+import { PasswordRequirements } from './PasswordRequirements';
 
 interface CredentialSectionProps {
   password: IPasswordField;
@@ -19,31 +20,36 @@ class CredentialSection extends React.Component<CredentialSectionProps> {
   render(){
     const { password, confirmation } = this.props;
     return (
-      <Fragment>
-        <Typography className={this.props.classes.newColor}>Coucou</Typography>
-        <TextField
-          required
-          label="Password"
-          variant="outlined"
-          type="password"
-          fullWidth={true}
-          value={password.value}
-          onChange={(e) => this.props.handleChange("password", e.target.value)}
-          {...(!password.isValid ? {error: true, helperText: password.error} : {})}
+      <FormControl component="fieldset" fullWidth={true}>
+        <FormLabel component="legend" style={{ margin: '1rem 0' }}>
+          Your credentials:
+        </FormLabel>
+        <FormGroup>
+          <TextField
+            required
+            label="Password"
+            variant="outlined"
+            type="password"
+            fullWidth={true}
+            value={password.value}
+            onChange={(e) => this.props.handleChange("password", e.target.value)}
+            {...(!password.isValid ? {error: true, helperText: password.error} : {})}
 
-          />
-        <TextField
-          required
-          label="Confirmation"
-          variant="outlined"
-          type="password"
-          fullWidth={true}
-          value={confirmation.value}
-          onChange={(e) => this.props.handleChange("confirmation", e.target.value)}
-          {...(!confirmation.isValid ? {error: true, helperText: confirmation.error} : {})}
-
-          />
-      </Fragment>)
+            />
+          <TextField
+            required
+            label="Confirmation"
+            variant="outlined"
+            type="password"
+            fullWidth={true}
+            value={confirmation.value}
+            onChange={(e) => this.props.handleChange("confirmation", e.target.value)}
+            {...(!confirmation.isValid ? {error: true, helperText: confirmation.error} : {})}
+            />
+          <PasswordRequirements password={password} />
+        </FormGroup>
+      </FormControl>
+    )
   }
 }
 
--- src/Login/components/IdentitySection.tsx
+++ src/Login/components/IdentitySection.tsx
@@ -1,5 +1,5 @@
-import { TextField } from '@material-ui/core';
-import React, { Fragment } from 'react';
+import { FormControl, FormGroup, FormLabel, TextField } from '@material-ui/core';
+import React from 'react';
 import { IFormField, RegisterFormKey } from '../types';
 
 interface IdentitySectionProps {
@@ -11,49 +11,42 @@ interface IdentitySectionProps {
 
 class IdentitySection extends React.Component<IdentitySectionProps> {
   render(){
-    // const isValid = true
-    // const errorDisplay = {
-    //   error: true,
-    //   helperText: "error"
-    // }
-    // const toto = {
-    //   label: "Email",
-    //   value: this.props.email,
-    //   onChange: (e: any) => this.props.handleChange("email", e.target.value)
-    // }
-
-    // Puis, a mettre dans le composant : {...(!isValid ? errorDisplay : {})}
     const { email, firstname, lastname } = this.props;
     return (
-      <Fragment>
-        <TextField
-          required
-          label="Email"
-          variant="outlined"
-          fullWidth={true}
-          value={email.value}
-          onChange={(e) => this.props.handleChange("email", e.target.value)}
-          {...(!email.isValid ? {error: true, helperText: email.error} : {})}
-          />
-        <TextField
-          required
-          label="Firstname"
-          variant="outlined"
-          fullWidth={true}
-          value={firstname.value}
-          onChange={(e) => this.props.handleChange("firstname", e.target.value)}
-          {...(!firstname.isValid ? {error: true, helperText: firstname.error} : {})}
-          />
-        <TextField
-          required
-          label="Lastname"
-          variant="outlined"
-          fullWidth={true}
-          value={lastname.value}
-          onChange={(e) => this.props.handleChange("lastname", e.target.value)}
-          {...(!lastname.isValid ? {error: true, helperText: lastname.error} : {})}
-          />
-      </Fragment>
+      <FormControl component="fieldset" fullWidth={true}>
+        <FormLabel component="legend" style={{ margin: '1rem 0' }}>
+          Your identity:
+        </FormLabel>
+        <FormGroup>
+          <TextField
+            required
+            label="Email"
+            variant="outlined"
+            fullWidth={true}
+            value={email.value}
+            onChange={(e) => this.props.handleChange("email", e.target.value)}
+            {...(!email.isValid ? {error: true, helperText: email.error} : {})}
+            />
+          <TextField
+            required
+            label="Firstname"
+            variant="outlined"
+            fullWidth={true}
+            value={firstname.value}
+            onChange={(e) => this.props.handleChange("firstname", e.target.value)}
+            {...(!firstname.isValid ? {error: true, helperText: firstname.error} : {})}
+            />
+          <TextField
+            required
+            label="Lastname"
+            variant="outlined"
+            fullWidth={true}
+            value={lastname.value}
+            onChange={(e) => this.props.handleChange("lastname", e.target.value)}
+            {...(!lastname.isValid ? {error: true, helperText: lastname.error} : {})}
+            />
+        </FormGroup>
+      </FormControl>
     )
   }
 }
