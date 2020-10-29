b9f51a327278fa7b01589e1f0332e7b3be701a97
daveguerra <david.guerradsc@gmail.com>
2020-10-29 11:34:56 +0100

    add new conversation

--- src/Users/components/UsersList.tsx
+++ src/Users/components/UsersList.tsx
@@ -7,21 +7,23 @@ import UsersListItem from './UsersListItem';
 
 interface UsersListProps {
   users: IUser[];
+  connectedUser?: IUser;
 }
 
 class UsersList extends React.Component<UsersListProps>{
-  render(){
-    if(this.props.users.length === 0){
+  render() {
+    if (this.props.users.length === 0) {
       return <h1>Loading</h1>
     } else {
       return <List>
-        {this.props.users.map((user, index) => <UsersListItem key={index} user={user} />)}
+        {this.props.users.map((user, index) => <UsersListItem key={index} user={user} connectedUser={this.props.connectedUser} />)}
       </List>
     }
   }
 }
 
-const mapStateToProps = ({users} : IAppState) => ({
-  users: users.list
-})
+const mapStateToProps = ({ users }: IAppState) => ({
+  users: users.list,
+  connectedUser: users.connectedUser
+});
 export default connect(mapStateToProps)(UsersList);
--- src/Users/components/UsersListItem.tsx
+++ src/Users/components/UsersListItem.tsx
@@ -1,21 +1,54 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 import { IUser } from '../types';
 import UserDetails from './UserDetails';
+import history from '../../history';
+import { IConversation } from '../../Chat/types';
+import { connect } from 'react-redux';
+import { updateConversation } from '../../Chat/actions/updateConversation';
 
 interface UsersListItemProps {
   user: IUser;
+  connectedUser?: IUser;
+
+  updateConversation: (conversation: IConversation) => void;
 }
 
 class UsersListItem extends React.Component<UsersListItemProps> {
+  createConversation = (user: IUser) => {
+    const { connectedUser } = this.props;
+    if (!connectedUser) { return }
+
+    // dispatch qui crée et ajoute une conversation dans la liste des conversations
+    const conversation: IConversation = {
+      _id: generateConversationId(connectedUser._id, user._id),
+      messages: [],
+      targets: [user._id, connectedUser._id],
+      updatedAt: new Date(),
+      unseenMessages: 0
+    }
+    // on l'ajoute a la liste en dispatchant la bonne action
+    this.props.updateConversation(conversation);
+    
+    history.push(`/conversation/${conversation._id}`)
+  }
+
   render() {
     const user = this.props.user;
     return (
-      <UserDetails id={user._id} />
+      <Button onClick={(_e) => this.createConversation(user)}>
+        <UserDetails id={user._id} />
+      </Button>
     )
   }
 }
 
+function generateConversationId(connectedUserId: string, targetId: string) {
+  return btoa(`${connectedUserId}_${targetId}_${new Date().toISOString()}`)
+}
 
+const mapDispatchToProps = (dispatch: any) => ({
+  updateConversation: (conversation: IConversation) => dispatch(updateConversation(conversation))
+});
 
-
-export default UsersListItem;
+export default connect(undefined, mapDispatchToProps)(UsersListItem);
