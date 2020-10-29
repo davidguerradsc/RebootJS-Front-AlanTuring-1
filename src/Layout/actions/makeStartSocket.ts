4be1c351b8fed82195dfc4d6a2114e831fc7576b
daveguerra <david.guerradsc@gmail.com>
2020-10-29 10:39:42 +0100

    fix bug

--- src/Layout/actions/makeStartSocket.ts
+++ src/Layout/actions/makeStartSocket.ts
@@ -1,20 +1,32 @@
 import io from 'socket.io-client';
+import { IAppState } from '../../appReducer';
+import { updateConversation } from '../../Chat/actions/updateConversation';
+import { IConversationMessage } from '../../Chat/types';
 import { makeUpdateUser } from '../../Users/actions/makeUpdateUser';
 import { IUser } from '../../Users/types';
 
-export function makeStartSocket(){
-  return (dispatch: any) => {
+export function makeStartSocket() {
+  return (dispatch: any, getState: () => IAppState) => {
     const socketServer = io.connect('http://localhost:3000');
 
     socketServer.on('connect', () => {
       console.log('Je suis bien connecté au back');
     })
 
-    socketServer.on('mon-super-event', (data: any) => {
-      console.log(data);
+    socketServer.on('new-message', ({ message }: { message: IConversationMessage }) => {
+      const conversations = getState().conversations.list;
+      const conversation = conversations.find(conv => conv._id === message.conversationId);
+      // TODO Quid quand la conv n'existe pas
+      if (!conversation) { return }
+
+      const newConversation = {
+        ...conversation,
+        messages: [...conversation.messages, message]
+      }
+      dispatch(updateConversation(newConversation));
     })
 
-    socketServer.on('user-status-update', ({user}: {user: IUser}) => {
+    socketServer.on('user-status-update', ({ user }: { user: IUser }) => {
       dispatch(makeUpdateUser(user));
     })
   }
